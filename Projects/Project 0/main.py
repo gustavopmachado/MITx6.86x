1532e4c6cc24c264033af577d5c57d6556e99966
Gustavo Machado <gustavo.machado04@yahoo.com.br>
2020-09-08 23:08:31 -0300

    1st set of exercises completed

--- Projects/Project 0/main.py
+++ Projects/Project 0/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def randomization(n):
     """
     Arg:
@@ -7,8 +8,10 @@ def randomization(n):
     Returns:
       A - a randomly-generated nx1 Numpy array.
     """
-    #Your code here
-    raise NotImplementedError
+
+    A = np.random.random_sample([n, 1])
+    return A
+
 
 def operations(h, w):
     """
@@ -23,8 +26,12 @@ def operations(h, w):
       B - a randomly-generated h x w Numpy array.
       s - the sum of A and B.
     """
-    #Your code here
-    raise NotImplementedError
+
+    A = np.random.random_sample([h, w])
+    B = np.random.random_sample([h, w])
+    s = A + B
+
+    return A, B, s
 
 
 def norm(A, B):
@@ -38,8 +45,10 @@ def norm(A, B):
     Returns:
       s - the L2 norm of A+B.
     """
-    #Your code here
-    raise NotImplementedError
+
+    s = np.linalg.norm(A+B)
+
+    return s
 
 
 def neural_network(inputs, weights):
@@ -53,20 +62,30 @@ def neural_network(inputs, weights):
      Returns (in this order):
        out - a 1 x 1 NumPy array, representing the output of the neural network
     """
-    #Your code here
+    # Your code here
     raise NotImplementedError
+
 
 def scalar_function(x, y):
     """
     Returns the f(x,y) defined in the problem statement.
     """
-    #Your code here
+    # Your code here
     raise NotImplementedError
+
 
 def vector_function(x, y):
     """
-    Make sure vector_function can deal with vector input x,y 
+    Make sure vector_function can deal with vector input x,y.
     """
-    #Your code here
+    # Your code here
     raise NotImplementedError
 
+
+def main():
+
+    pass
+
+	
+if __name__ == "__main__":
+    main()
