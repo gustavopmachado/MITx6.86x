e75cf9d3d72b59fa1bcbf469fec85a406c0cc519
Gustavo Machado <gustavo.machado04@yahoo.com.br>
2020-10-31 10:36:12 -0300

    ReLU implementation

--- Projects/Digit Recognition (Project 2-3)/part2-nn/neural_nets.py
+++ Projects/Digit Recognition (Project 2-3)/part2-nn/neural_nets.py
@@ -15,11 +15,11 @@ import math
 
 def rectified_linear_unit(x):
     """ Returns the ReLU of x, or the maximum between 0 and x."""
-    # TODO
+    return np.maximum(0, x)
 
 def rectified_linear_unit_derivative(x):
     """ Returns the derivative of ReLU."""
-    # TODO
+    return np.where(x <= 0, 0, 1)
 
 def output_layer_activation(x):
     """ Linear function, returns input as is. """
